bc700daf41568288e8fb16c07a0cdae4c23a4520
JFreyra <efreyra@stuy.edu>
2016-12-09 00:50:59 -0500

    seatgeek works, byPrice works

--- utils/seatgeek.py
+++ utils/seatgeek.py
@@ -1,19 +1,82 @@
-from flask import Flask, render_template
 import urllib2, json, requests
 from requests import auth
 from requests.auth import HTTPBasicAuth
 
-app = Flask(__name__)
+#
+#    Input: Single word query
+#  Returns: List of events relevant to the query in New York and information separated in a list of entries
+#
+#           [organization, event, performance, url, time, ticket type, price, seat]
+#
+def seatgeek(query):
 
-@app.route("/")
-def root():
-    u = urllib2.urlopen("https://api.seatgeek.com/2/events?venue.state=NY")
+    fullList = []
+
+    link = "https://api.seatgeek.com/2/events?q="+query.replace(" ","+")+"&venue.state=NY"
+    u = urllib2.urlopen(link)
     response = u.read()
     data = json.loads(response)
-    print(data['events'][0])
-    #return render_template("index.html", pic = data['url'] )
+
+    events = data['events']
+    
+    for i in events:
+        
+        orgName = "N/A"
+        eventName = i['title']
+        url = i['url']
+        time = i['datetime_utc']
+        ticket_type = "N/A"
+            
+        seat = "N/A"
+
+        for j in i['performers']:
+
+            
+            price = "N/A"
+        
+            try:
+                float(i['stats']['average_price'])
+                price = i['stats']['average_price'] # seatgeek does not show ticket types nor specific prices or seats
+            except:
+                price = 999999
+
+            
+            entry = []
+            perfName = j['name']
+
+            entry.append(orgName)
+            entry.append(eventName)
+            entry.append(perfName)
+            entry.append(url)
+            entry.append(time)
+            entry.append(ticket_type)
+            entry.append(price)
+            entry.append(seat)
+
+            fullList.append(entry)
+
+    return fullList
 
 
-if __name__ == "__main__":
-   app.debug = True
-   app.run()
+def byPrice(query):
+
+    orderedList = []
+    fullList = seatgeek(query)
+
+    orderedList = sorted(fullList, key=lambda entry: float(entry[6]))
+    
+    for i in orderedList:
+        if i[6] == 999999:
+            i[6] = "N/A: Price not Defined"
+    
+    return orderedList
+
+
+## TESTING ##
+
+# def main():
+#     listF = byPrice("music")
+#     for i in listF:
+#         print(i[6])
+
+# main()
