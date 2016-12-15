55998c227063650c96fea08c143c46c3612d8b49
Jason Mohabir <jmohabir@stuy.edu>
2016-12-14 21:22:23 -0500

    I forgot to *add

---
+++ utils/keyReader.py
@@ -0,0 +1,9 @@
+import csv
+
+def returnKey(apiName):
+    with open('keys.csv', 'rb') as csvfile:
+        keyreader = csv.reader(csvfile)
+
+        for row in keyreader:
+            if row[0] == apiName:
+                return(row[1])
